f18cb9ba3f804abc6f14675e109a1d99003316d2
acer <acer@DESKTOP-F4207EA>
2022-11-03 22:16:09 +0530

    added variable

--- demoproject/src/demoproject/hello.java
+++ demoproject/src/demoproject/hello.java
@@ -5,7 +5,10 @@ public class hello {
 public static void main(String args[])
 {
 	System.out.println("hai , hello world");
-	
-	
+    System.out.println("manikandan");
+    int a =10;
+    int b = a + 5;
+    System.out.println("the result is:" + b);
+    	
 }
 }
